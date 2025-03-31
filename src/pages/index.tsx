5a4c72768c628baea25b050af4014a6c4d6402dd
Kevin Avila <hola@kevinavila.pro>
2025-03-31 13:35:59 -0600

    gatsby feat: Add tailwind@3

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -139,7 +139,7 @@ const links = [
 const IndexPage: React.FC<PageProps> = () => {
   return (
     <main style={pageStyles}>
-      <h1 style={headingStyles}>
+      <h1 className="text-3xl text-red-500">
         Congratulations
         <br />
         <span style={headingAccentStyles}>— you just made a Gatsby site! 🎉🎉🎉</span>
