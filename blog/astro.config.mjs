a04b06635a48ff9daa8b4eff7b342b4baa14ec44
Kevin Avila <hola@kevinavila.pro>
2025-04-22 12:15:19 +0200

    astro feat: Migrate to astro

---
+++ blog/astro.config.mjs
@@ -0,0 +1,18 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  i18n: {
+    locales: ['en-US', 'es-ES'],
+    defaultLocale: 'en-US',
+    routing: 'manual',
+  },
+  vite: {
+    resolve: {
+      alias: {
+        '@components': '/src/components'
+      }
+    }
+  }
+});
