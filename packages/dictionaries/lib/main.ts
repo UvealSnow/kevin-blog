2bcd81fb94fc2abd3375a750fb56fcaf37da46b2
Kevin Avila <hola@kevinavila.pro>
2025-04-28 12:45:01 +0200

    astro chore: format main.ts for consistent indentation and style in dictionary module

--- packages/dictionaries/lib/main.ts
+++ packages/dictionaries/lib/main.ts
@@ -5,25 +5,25 @@ interface LocaleDictionary {
 }
 
 interface Module {
-  default: LocaleDictionary
+	default: LocaleDictionary
 }
 
 export const getDictionary = (site: string): Dictionary => {
-  const dictionaries: Dictionary = {}
+	const dictionaries: Dictionary = {}
 
-  // @ts-ignore
-  const modules = import.meta.glob('./i18n/**/*.json', {eager: true})
+	// @ts-ignore
+	const modules = import.meta.glob('./i18n/**/*.json', { eager: true })
 
-  for (const path in modules) {
-    if (path.includes(site)) {
-      const matches = path.match(/[a-z]{2}-[A-Z]{2}/)
-      if (matches != null) {
-        const locale = matches[0] as string
-        const module = modules[path] as Module;
-        dictionaries[locale] = module.default as LocaleDictionary
-      }
-    }
-  }
+	for (const path in modules) {
+		if (path.includes(site)) {
+			const matches = path.match(/[a-z]{2}-[A-Z]{2}/)
+			if (matches != null) {
+				const locale = matches[0] as string
+				const module = modules[path] as Module
+				dictionaries[locale] = module.default as LocaleDictionary
+			}
+		}
+	}
 
-  return dictionaries
+	return dictionaries
 }
