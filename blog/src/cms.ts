aaf3c03bf0d12a87ee8addedbd8a95680d3c3f41
Kevin Avila <hola@kevinavila.pro>
2025-04-28 17:07:14 +0200

    astro feat: add asset utility function for handling asset URLs and refactor CMS URL handling for improved clarity

--- blog/src/cms.ts
+++ blog/src/cms.ts
@@ -2,6 +2,8 @@ type NestedRecord = {
 	[key: string]: string | string[] | NestedRecord | NestedRecord[]
 }
 
+const BASE_URL = process.env.CMS_URL ?? 'http://localhost:8055'
+
 const appendSearchParams = (url: URL, record: NestedRecord): URL => {
 	for (let [key, value] of Object.entries(record)) {
 		if (typeof value === 'object' && !Array.isArray(value)) {
@@ -20,16 +22,25 @@ const appendSearchParams = (url: URL, record: NestedRecord): URL => {
 
 const get = async <T>(path: string, query: NestedRecord): Promise<{ data: T }> => {
 	try {
-		const BASE_PATH = process.env.CMS_URL ?? 'http://localhost:8055'
-		const targetURL = appendSearchParams(new URL(path, BASE_PATH), query)
-
-		console.log(targetURL.toString())
-
+		const targetURL = appendSearchParams(new URL(path, BASE_URL), query)
 		const raw = await fetch(targetURL)
 		return raw.json()
 	} catch (error) {
+    console.error(error)
 		return Promise.reject(error)
 	}
 }
 
-export { get }
+const asset = (uri?: null | string): URL => {
+	if (!uri) {
+		return new URL('https://placehold.co/100')
+	}
+
+	if (/^https?:\/\//i.test(uri)) {
+		return new URL(uri)
+	}
+
+	return new URL('/assets/' + uri, BASE_URL)
+}
+
+export { get, asset }
